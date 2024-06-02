2be64987fae60547985302b09e7ec59db637ce42
Tomislav Dukez <tomdu3@ymail.com>
2024-06-02 22:51:37 +0200

    Add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1>TDCode Portfolio</h1>
-        <p>Full-stack developer and Data Scientist portfolio</p>
-        <p>
-          <a href="https://github.com/tdcode">Github</a>
-        </p>
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+        />
+        <Hero />
       </div>
     </main>
   );
