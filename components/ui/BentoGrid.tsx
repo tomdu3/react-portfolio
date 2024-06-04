2fcbd9d3b336a9d8c642cc99f01faf3f4fe3d5d2
Tomislav Dukez <tomdu3@ymail.com>
2024-06-04 19:02:20 +0200

    Fix tech skills card

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -45,7 +45,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1]",
         className
       )}
       style={{
@@ -94,23 +94,39 @@ export const BentoGridItem = ({
         <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
           {title}
         </div>
-        {id === 2 && <GlobeDemo />}
-        {id === 3 && (
-          <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-            <div className="flex flex-col gap-3 lg:gap-8">
-              {['Django', 'Flask', 'Jekyll', 'Tableau'].map((item) => (
-                <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">{item}</span>
-              ))}
-              <span className="py-4 pc-3 rounded-lg text-center bg-[#10132E]" />
-            </div>
-            <div className="flex flex-col gap-3 lg:gap-8">
-              {['Vue.js', 'AWS', 'MongoD'].map((item) => (
-                <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">{item}</span>
-              ))}
-              <span className="py-4 pc-3 rounded-lg text-center bg-[#10132E]" />
-            </div>
-          </div>
-        )}
+         {/* for the github 3d globe */}
+         {id === 2 && <GlobeDemo />}
+
+{/* Tech stack list div */}
+{id === 3 && (
+  <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+    {/* tech stack lists */}
+    <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+      {['Flask', 'Django', 'Postgres'].map((item, i) => (
+        <span
+          key={i}
+          className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+          lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+        >
+          {item}
+        </span>
+      ))}
+      <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+    </div>
+    <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+      <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+      {['Tableau', 'Scikit-Learn', 'Pandas'].map((item, i) => (
+        <span
+          key={i}
+          className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+          lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+        >
+          {item}
+        </span>
+      ))}
+    </div>
+  </div>
+)}
       </div>
     </div>
   );
