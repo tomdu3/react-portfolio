376760d0cecc5e52b31689129ab5caded710eb8a
Tomislav Dukez <tomdu3@ymail.com>
2024-06-04 18:02:48 +0200

    add globe and tech stack

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,8 @@
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { GlobeDemo } from "./GridGlobe";
+import { div } from "three/examples/jsm/nodes/Nodes.js";
+
 
 export const BentoGrid = ({
   className,
@@ -42,7 +45,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 ",
+        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
       )}
       style={{
@@ -51,40 +54,63 @@ export const BentoGridItem = ({
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
-      <div
-        className={`${id === 6} && 'flex justify-center h-full'`}
-      >
+      <div className={`${id === 6} && 'flex justify-center h-full'`}>
         <div className="w-full h-full absolute">
           {img && (
             <img
               src={img}
               alt={img}
-              className={cn(imgClassName, 'object-cover, object-center')}
+              className={cn(imgClassName, "object-cover, object-center")}
             />
           )}
         </div>
         <div className="{`absolute right-0 -bottom-5 ${id === 5} && 'w-full opacity-80'}`}">
-          {spareImg  && (
+          {spareImg && (
             <img
               src={spareImg}
               alt={spareImg}
-              className={cn(imgClassName, 'object-cover, object-center w-full h-full')}
+              className={cn(
+                imgClassName,
+                "object-cover, object-center w-full h-full"
+              )}
             />
           )}
-          </div>
-         {id === 6 && (
-           <BackgroundGradientAnimation>
+        </div>
+        {id === 6 && (
+          <BackgroundGradientAnimation>
             <div className="absolute z-50 flex items-center justify-center text-white font-bold" />
-           </BackgroundGradientAnimation>
-         )}
+          </BackgroundGradientAnimation>
+        )}
       </div>
-      <div className={cn(titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10')}>
-      <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
+      <div
+        className={cn(
+          titleClassName,
+          "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+        )}
+      >
+        <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
           {description}
         </div>
         <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
           {title}
         </div>
+        {id === 2 && <GlobeDemo />}
+        {id === 3 && (
+          <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+            <div className="flex flex-col gap-3 lg:gap-8">
+              {['Django', 'Flask', 'Jekyll', 'Tableau'].map((item) => (
+                <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">{item}</span>
+              ))}
+              <span className="py-4 pc-3 rounded-lg text-center bg-[#10132E]" />
+            </div>
+            <div className="flex flex-col gap-3 lg:gap-8">
+              {['Vue.js', 'AWS', 'MongoD'].map((item) => (
+                <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">{item}</span>
+              ))}
+              <span className="py-4 pc-3 rounded-lg text-center bg-[#10132E]" />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
