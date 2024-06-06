b75e19d4793876da83ec005c5cd350c516633364
Tomislav Dukez <tomdu3@ymail.com>
2024-06-06 17:38:01 +0200

    add connect data with RecentProjects component

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import { projects } from "@/data";
+import { title } from "process";
+import React from "react";
 
 const RecentProjects = () => {
   return (
     <div className="py-20">
-        <h1 className='heading'>
-            A small selection of {' '}
-            <span className='text-purple'>recent projects</span>
-        </h1>
+      <h1 className="heading">
+        A small selection of{" "}
+        <span className="text-purple">recent projects</span>
+      </h1>
+      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+        {projects.map(({ id, title, des, img, iconLists, link }) => (
+          <div key={id} className="lg:min-h-[32.5rem] flex items-center justify-center sm:w-96 w-[80vw]">
+            {title}
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RecentProjects
+export default RecentProjects;
